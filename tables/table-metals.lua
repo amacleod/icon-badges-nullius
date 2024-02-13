7dbfe80cb4aef938f9e843c1a22697fd4a380807
Allister MacLeod <allister.macleod@gmail.com>
2024-02-13 18:40:03 -0500

    Adding iron ore and bauxite ore as examples.
    Mostly for myself for later :D

--- tables/table-metals.lua
+++ tables/table-metals.lua
@@ -1,4 +1,8 @@
 return [[
 badge;name;type;badgeset
-"";"";"";""
+"Al";"nullius-bauxite";"item";"metals-set-english"
+"I";"iron-ore";"item";"metals-set-english"
+
+"Al";"nullius-bauxite","item";"metals-set-latin"
+"Fe";"iron-ore";"item";"metals-set-latin"
 ]]
